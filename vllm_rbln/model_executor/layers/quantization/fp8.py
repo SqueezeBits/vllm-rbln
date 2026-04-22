a890afcd441e6c1d03b2d6f84c6801d6254f8174
rebel-kblee <119555851+rebel-kblee@users.noreply.github.com>
2026-04-22 15:05:35 +0900

    fix(model): make expert_map with torch export in Moe model (#537)

--- vllm_rbln/model_executor/layers/quantization/fp8.py
+++ vllm_rbln/model_executor/layers/quantization/fp8.py
@@ -863,10 +863,8 @@ class Fp8MoEMethod(FusedMoEMethodBase):
 
         expert_map_const = None
         if layer.expert_map is not None:
-            expert_map_const = layer.expert_map
-            if expert_map_const.dtype != torch.int32:
-                expert_map_const = expert_map_const.to(dtype=torch.int32)
-            expert_map_const = expert_map_const.detach().clone()
+            assert getattr(layer, "expert_map_const", None) is not None
+            expert_map_const = torch.tensor(layer.expert_map_const, dtype=torch.int32)
 
         tokens_mask = None
         use_moe_tokens_mask = envs.VLLM_RBLN_USE_MOE_TOKENS_MASK
